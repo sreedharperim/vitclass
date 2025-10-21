dd5734b15d429648b5d2892f44d5c2aa74858638
sreedharperim <sreedharperim@yahoo.com>
2025-10-21 18:37:34 +0530

    Update App.js - Replaced BrowserRouter with HashRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
@@ -19,4 +19,4 @@ import TeacherDashboard from './pages/TeacherDashboard';
 import AssignmentSubmissions from './pages/AssignmentSubmissions';
 import AssignmentEdit from './pages/AssignmentEdit';
 
-export default function App(){ return (<AuthProvider><BrowserRouter><Navbar /><Routes><Route path='/' element={<Dashboard/>} /><Route path='/login' element={<Login/>} /><Route path='/register' element={<Register/>} /><Route path='/assignments' element={<Assignments/>} /><Route path="/dashboard" element={<TeacherDashboard />} /><Route path="/assignments/:id" element={<AssignmentDetail />} /><Route path='/grades' element={<Grades/>} /><Route path='/class/:id' element={<ClassPage/>} /><Route path="/assignments/:id/submissions" element={<AssignmentSubmissions />} /><Route path="/assignments/:id/edit" element={<AssignmentEdit />} /><Route path='/class/:id/create' element={<CreateAssignment/>} /><Route path='/class/:id/roster' element={<RosterPage/>} /><Route path="/messages" element={<MessagesPage/>} /><Route path="/messages/:id" element={<MessageDetail />} /></Routes></BrowserRouter></AuthProvider>) }
+export default function App(){ return (<AuthProvider><HashRouter><Navbar /><Routes><Route path='/' element={<Dashboard/>} /><Route path='/login' element={<Login/>} /><Route path='/register' element={<Register/>} /><Route path='/assignments' element={<Assignments/>} /><Route path="/dashboard" element={<TeacherDashboard />} /><Route path="/assignments/:id" element={<AssignmentDetail />} /><Route path='/grades' element={<Grades/>} /><Route path='/class/:id' element={<ClassPage/>} /><Route path="/assignments/:id/submissions" element={<AssignmentSubmissions />} /><Route path="/assignments/:id/edit" element={<AssignmentEdit />} /><Route path='/class/:id/create' element={<CreateAssignment/>} /><Route path='/class/:id/roster' element={<RosterPage/>} /><Route path="/messages" element={<MessagesPage/>} /><Route path="/messages/:id" element={<MessageDetail />} /></Routes></HashRouter></AuthProvider>) }
